e171f8b5e142b4d9a45100ae3aeb9ba413aff49d
Mevericks <reloved@gmail.com>
2019-02-03 17:45:54 +0200

    one-to-one relation onDelete

--- database/migrations/2019_02_03_151233_create_profiles_table.php
+++ database/migrations/2019_02_03_151233_create_profiles_table.php
@@ -16,11 +16,10 @@ class CreateProfilesTable extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('url');
             $table->string('telephone');
             $table->timestamps();
-
         });
     }
 

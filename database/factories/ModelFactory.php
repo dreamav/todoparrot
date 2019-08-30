0df3a018b17ed09f13e3654f3c7d20356089cb2a
Mevericks <reloved@gmail.com>
2019-08-30 19:37:06 +0300

    faker generator

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+    	'user_id' => function(){
+    		return factory(App\User::class)->create()->id;
+    	},
+        'title' => $faker->sentence,
+        'body' => $faker->sentence
+    ];
+});
+
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+    $slug = $faker->word;
+    return [
+        'name' => ucwords($slug),
+        'slug' => $slug
+    ];
+});

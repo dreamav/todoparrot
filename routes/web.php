d36beaca12f8f4b0a856680914b4f1de95f337c5
Mevericks <reloved@gmail.com>
2019-08-30 19:33:24 +0300

    роуты

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,34 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
+Route::get('/', function (){
+    $user = App\User::first();
+//    $post = $user->posts()->create([
+//        'title'=>'foobar',
+//        'body'=>'loremipsum'
+//    ]);
+//    $post->tags()->attach(1);
+
+    $posts = $user->posts;
+    foreach ($posts as $post){
+        $tags = $post->tags;
+            foreach ($tags as $tag) {
+                echo $tag->name;
+            }
+    }
+
+//    $users = App\User::where('id', 1)->get();
+//
+//    foreach ($users as $user) {
+//        echo $user->name;
+//    }
+
+    return view('home');
+});
 
 Route::get('lists/category/{category}', 'ListsController@category');
 Route::resource('lists', 'ListsController');
+Auth::routes();
+
+Route::get('/home', ["uses"=>'HomeController@index',"as"=>"home","middleware"=>"mymiddle"]);
